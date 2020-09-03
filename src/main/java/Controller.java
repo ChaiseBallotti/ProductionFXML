cd162641ef94cc0336a912d5236d5cc9a13a0e19
ChaiseBallotti <ballottic@gmail.com>
2020-09-02 21:01:52 -0400

    Cleaned up GUI and removed unneeded files

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,46 +1,16 @@
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Tab;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Button;
 
-public class Controller implements Initializable {
-  @FXML
-  Tab productLine;
-  @FXML
-  Tab produce;
-  @FXML
-  Tab productionLog;
-  @Override
-  public void initialize(URL location, ResourceBundle resources) {
-    FXMLLoader loader = new FXMLLoader();
-    try {
-      AnchorPane anchor1 = loader.load(getClass().getResource("ProductLine.fxml"));
-      productLine.setContent(anchor1);
-    } catch (IOException iex) {
-      System.out.println("unable to load ProductLine");
-    }
+public class Controller {
 
-    loader = new FXMLLoader();
-    try {
-      AnchorPane anchor2 = loader.load(getClass().getResource("Produce.fxml"));
-      produce.setContent(anchor2);
-    } catch (IOException iex) {
-      System.out.println("unable to load Produce");
-    }
+  @FXML
+  private Button addProductButton;
+  private Button recordProductionButton;
 
-    loader = new FXMLLoader();
-    try
-    {
-      AnchorPane anchor3 = loader.load(getClass().getResource("ProductionLog.fxml"));
-      productionLog.setContent(anchor3);
-    }
-    catch(IOException iex)
-    {
-      System.out.println("unable to load ProductionLog");
-    }
+  @FXML
+  public void testOut(ActionEvent event){
+    System.out.println("Button pushed");
   }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -16,7 +15,6 @@ public class Main extends Application {
     Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
     Scene scene = new Scene(root, 800, 500);
-    //scene.getStylesheets().add(getClass().getResource("java\\Steel.css").toExternalForm());
     primaryStage.setTitle("Vinson Dynamics");
     primaryStage.setScene(scene);
     primaryStage.show();
